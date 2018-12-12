5da46fc2e5dfe943d59dbe16e54db9d041059c87
RJ-1998 <jrishabh76@gmail.com>
2018-12-12 23:59:31 +0530

    first commit

---
+++ precache-manifest.15e9374ed63770ce047a212a7b649bf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2229421287b5882668ea",
+    "url": "/static/js/main.22294212.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "2229421287b5882668ea",
+    "url": "/static/css/main.f2f1c587.chunk.css"
+  },
+  {
+    "revision": "f0605715df4493ba989ac62ef70382dd",
+    "url": "/index.html"
+  }
+];
